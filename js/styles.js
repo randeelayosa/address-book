78383e56af4d80617dc47dd887a894d7e401d8be
Randee Layosa <randeelayosa@Randees-iMac.local>
2019-01-28 02:24:08 -0800

    Redo the previous commit. Forgot to save contents.

---
+++ js/styles.js
@@ -0,0 +1,51 @@
+//Business Logic for AddressBook -------------
+function AddressBook() {
+  this.contacts = [],
+  this.currentId = 0
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contaacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+//Business Logic for Contacts -------------
+//Creating a Contact constructor:
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName,
+  this.lastName = lastName,
+  this.phoneNumber = phoneNumber
+}
+
+//Creating a prototype method on our constructor:
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
